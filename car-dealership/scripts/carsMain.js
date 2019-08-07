c4346e86958327ef309db8b83cabc6afafec18c9
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-08-07 16:01:25 -0500

    working on month count for sales

--- car-dealership/scripts/carsMain.js
+++ car-dealership/scripts/carsMain.js
@@ -21,6 +21,37 @@ console.log("Gross Profit for 2017:", grossProfit2017)
 
 // In which month did they sell the most cars?
 
+const d = new Date()
+console.log(d)
+
+const getPurchaseDates = []
+
+carsSold2017.forEach(car => {
+    const purchaseDate = car.purchase_date
+    const monthsList = purchaseDate.split("-")[1]
+    getPurchaseDates.push(monthsList)  
+})
+
+console.log(getPurchaseDates)
+// Use a hash table
+// Create an empty object
+const monthCount = {}
+
+// Iterate over the array of months
+// Check to see if the month exists as a key in the object
+    // If the month does not exist, add it as a key and set the value to 1
+    // If the month exists as a key in the object, increment the count by 1
+
+// getPurchaseDates.forEach(month => {
+//     if (!monthCount.includes(month)) {
+//         monthCount.monthNumber = month
+//         monthCount.value = 1
+//     } else {
+//         monthCount.value++
+//     }
+// })
+
+// console.log(monthCount)
 
 // Which salesperson sold the most cars?
 

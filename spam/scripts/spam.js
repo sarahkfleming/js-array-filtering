58f17a8e43247ce93a65084bcb291d3768a29419
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-08-07 12:27:54 -0500

    finished spam exercise

--- spam/scripts/spam.js
+++ spam/scripts/spam.js
@@ -4,9 +4,12 @@ import customers from "./customerData.js"
 
 // From the customers array, extract just the customers' contact email addresses and store them in a new array. You will need a nested array method - meaning one iteration inside another one - since you need to iterate the entire array of customers, and then iterate the array of emails for each one.
 
-const customerEmailAddresses = customers.forEach(customer => {
+const customerEmailAddresses = []
+
+customers.forEach(customer => {
     customer.contacts.email.map(emailAddress => {
-        return emailAddress
+        customerEmailAddresses.push(emailAddress)
+        return customerEmailAddresses
     })
 })
 
